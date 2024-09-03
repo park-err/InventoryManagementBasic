0304a1f84c89536fe700ff646919d67a2081bcd7
Parker <94702972+park-err@users.noreply.github.com>
2024-09-02 20:52:15 -0500

    fixing bug with pull requests

--- InventoryBasic2/InventoryBasic2/InvMngmt/ConsoleAppMenu.cs
+++ InventoryBasic2/InventoryBasic2/InvMngmt/ConsoleAppMenu.cs
@@ -9,33 +9,25 @@ namespace InventoryBasic2.InvMngmt
         {
             int temp1; int temp2;
             Items items = new Items();
-            char keepGoing = 'y';
-            while (keepGoing == 'y')
+                
+            Console.WriteLine("Pick one:\n");
+            Console.WriteLine("	1. Create item\n");
+            Console.WriteLine("	2. Check inventory\n");
+            Console.WriteLine("	3. Check sales\n");
+            Console.WriteLine("	4. Import sales\n");
+            Console.WriteLine("	5. Export inventory\n");
+
+            string input = Console.ReadLine();
+                
+            switch (input)
             {
-                Console.WriteLine("Pick one:\n");
-                Console.WriteLine("	1. Create item\n");
-                Console.WriteLine("	2. Check inventory\n");
-                Console.WriteLine("	3. Check sales\n");
-                Console.WriteLine("	4. Import sales\n");
-                Console.WriteLine("	5. Export inventory\n");
-
-                char input = (char)Console.Read();
-                temp1 = Console.Read();
-                temp2 = Console.Read();
-
-                switch (input)
-                {
-                    case '1': items.CreateItem(init); break;
-                    case '2': Console.WriteLine(input); break;
-                    case '3': Console.WriteLine(input); break;
-                    case '4': Console.WriteLine(input); break;
-                    default: Console.WriteLine(input); break;
-                }
-
-                keepGoing = (char)Console.Read();
-                temp1 = Console.Read();
-                temp2 = Console.Read();
+                case "1": items.CreateItem(init); break;
+                case "2": Console.WriteLine(input); break;
+                case "3": Console.WriteLine(input); break;
+                case "4": Console.WriteLine(input); break;
+                default: Console.WriteLine(input); break;
             }
         }
     }
 }
+
